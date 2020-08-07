da92a8a88f0da91c613630c48da0710a8c0b03a5
mhhafezi <68470247+mhhafezi@users.noreply.github.com>
2020-08-08 00:19:38 +0430

    Add files via upload

--- T1_2.py
+++ T1_2.py
@@ -1,1 +1,33 @@
-print('Hi')
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+lr_model = LinearRegression(fit_intercept=True, normalize=True)
+#k=10
+for count, i in enumerate([5, 10, 25, 100]):
+    x = np.random.rand(i, 1) * 20
+    x = x.reshape(-1, 1)
+    y = 2.358 * x - 3.121
+    y = y + np.random.normal(scale=3, size=y.shape)
+    y = y.reshape(-1, 1)
+    lr_model.fit(x, y)
+    h = lr_model.predict(x)
+    MSE = np.mean((y - h) ** 2) / 2
+    print("MSE = ", 1 - (2 * MSE / y.var()))
+    MSE_list = []
+    MSE_list.append(MSE)
+
+    for j in range(1, 11):
+        x = np.hstack((x, x**(j+1)))
+        y = 2.358 * x - 3.121
+        y = y + np.random.normal(scale=3, size=y.shape)
+        y = y.reshape(-1, 1)
+        lr_model.fit(x, y)
+        h = lr_model.predict(x)
+        MSE = np.mean((y-h)**2) / 2
+        print("MSE = ", 1-(2*MSE / y.var()))
+        MSE_list.append(MSE)
+        plt.plot(range(1,11), MSE_list, 'bo')
+        plt.xlabel("Power")
+        plt.ylabel('MSE')
+        plt.show()
